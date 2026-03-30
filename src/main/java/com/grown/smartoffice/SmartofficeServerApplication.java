eb6b593643187f3329c8a233049a19e1ecef8853
sjparkx1129 <sjparkx1129@gmail.com>
2026-03-30 10:22:23 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/grown/smartoffice/SmartofficeServerApplication.java
@@ -0,0 +1,13 @@
+package com.grown.smartoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartofficeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartofficeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grown/smartoffice/SmartofficeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grown.smartoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartofficeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
